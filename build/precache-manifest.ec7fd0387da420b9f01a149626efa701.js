619b1dd074041049d9a9382c528ffb605ca4724e
Donovan K <donovankutzer@gmail.com>
2020-04-25 15:42:26 -0700

    Added production build for easy execution

---
+++ build/precache-manifest.ec7fd0387da420b9f01a149626efa701.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04c5d75cde581db20b213666618459d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc081e3f74cb8f47ef3b",
+    "url": "/static/css/main.d8880a99.chunk.css"
+  },
+  {
+    "revision": "d737281f91db7c4776be",
+    "url": "/static/js/2.a2d0f2ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a2d0f2ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc081e3f74cb8f47ef3b",
+    "url": "/static/js/main.ec83667a.chunk.js"
+  },
+  {
+    "revision": "336e5efbe456469a5140",
+    "url": "/static/js/runtime-main.08ae421d.js"
+  }
+]);
